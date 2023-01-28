82e9eb08358257b6f7e30c2b65722bdaccabe545
Jake Mills <jakemills@Jakes-Air.broadband>
2023-01-28 11:55:16 +0000

    added mesh background

---
+++ scripts.js
@@ -0,0 +1,25 @@
+import { Gradient } from './gradient.js'
+
+const gradientCanvas = document.querySelector('#gradient-canvas')
+
+console.log(gradientCanvas);
+
+const gradients = [
+'gradient-one', 'gradient-two', 'gradient-three'
+]
+
+const setColours = () => {
+  const index = Math.floor(Math.random() * (gradients.length))
+  const colours = gradients[index]
+  console.log(index, colours)
+  gradientCanvas.classList.add(colours)
+}
+
+setColours()
+
+// Create your instance
+const gradient = new Gradient() 
+
+// Call `initGradient` with the selector to your canvas
+gradient.initGradient('#gradient-canvas')
+
