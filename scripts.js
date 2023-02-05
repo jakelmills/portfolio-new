0ec57ad6058d2b6f70e6335852e9ce0e9268dc27
Jake Mills <jakemills@Jakes-MacBook-Air.local>
2023-02-05 15:00:16 +0000

    added scripting to add/remove classes, added project images and description

--- scripts.js
+++ scripts.js
@@ -2,5 +2,61 @@ import { Gradient } from './gradient.js'
 
 const gradientCanvas = document.querySelector('#gradient-canvas')
 
+const projectOne = document.querySelector('.project.project-one')
+const projectOneHover = document.querySelector('.project-one .project-hover-area')
+const projectOneMinimise = document.querySelector('.project-one .minimise-btn')
+
+const projectTwo = document.querySelector('.project-two')
+const projectTwoHover = document.querySelector('.project-two .project-hover-area')
+const projectTwoMinimise = document.querySelector('.project-two .minimise-btn')
+
+const projects = document.querySelectorAll('.projects')[0].children
+
 const gradient = new Gradient() 
 gradient.initGradient('#gradient-canvas')
+
+
+const expandProject = (currentProject) => {
+  for (let i=0; i < projects.length; i++){
+    projects[i].classList.remove('expanded')
+  }
+  currentProject.classList.add('expanded')
+}
+
+projectOneHover.addEventListener('mouseover', () => {
+  projectOne.style.scale = 0.98
+})
+
+projectOneHover.addEventListener('mouseout', () => {
+  projectOne.style.scale = 1
+})
+
+projectTwoHover.addEventListener('mouseover', () => {
+  projectTwo.style.scale = 0.98
+})
+
+projectOneHover.addEventListener('click', () => {
+  projectOneHover.classList.add('hidden')
+  projectTwo.scrollTo(0, 0)
+  projectTwoHover.classList.remove('hidden')
+  expandProject(projectOne)
+})
+
+projectOneMinimise.addEventListener('click', () => {
+  projectOne.classList.remove('expanded')
+})
+
+projectTwoHover.addEventListener('click', () => {
+  projectTwoHover.classList.add('hidden')
+  projectOne.scrollTo(0, 0)
+  projectOneHover.classList.remove('hidden')
+  expandProject(projectTwo)
+})
+
+projectTwoHover.addEventListener('mouseout', () => {
+  projectTwo.style.scale = 1
+})
+
+projectTwoMinimise.addEventListener('click', () => {
+  projectTwo.classList.remove('expanded')
+})
